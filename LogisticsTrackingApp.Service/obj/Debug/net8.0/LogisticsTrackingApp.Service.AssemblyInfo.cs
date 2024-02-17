656396bd53015dd97a8a6bb37b75723510e08e98
ckmkomer <101353123+ckmkomer@users.noreply.github.com>
2024-02-17 15:56:44 +0300

    configuration

--- LogisticsTrackingApp.Service/obj/Debug/net8.0/LogisticsTrackingApp.Service.AssemblyInfo.cs
+++ LogisticsTrackingApp.Service/obj/Debug/net8.0/LogisticsTrackingApp.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogisticsTrackingApp.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eca2a07e529d7ddb41927fe57e29939f51348739")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogisticsTrackingApp.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogisticsTrackingApp.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
